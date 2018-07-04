461be643bef8c18c438c852c0e51bd4e31833d83
laowang <>
2018-07-04 16:54:38 +0800

    add datepicker filter date for list order

--- src/main/java/com/duofuen/repair/service/UserService.java
+++ src/main/java/com/duofuen/repair/service/UserService.java
@@ -182,12 +182,11 @@ public class UserService {
 
     private String generatePassword() {
         String[] str = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-                "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+                "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
         Random random = new Random(System.currentTimeMillis());
         StringBuilder verifyBuilder = new StringBuilder();
         for (int i = 0; i < 8; i++) {
-            int rd = random.nextInt(62);
+            int rd = random.nextInt(str.length);
             verifyBuilder.append(str[rd]);
         }
         String password = verifyBuilder.toString();
